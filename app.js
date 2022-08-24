6bb75a0568de46bb34df2b52626060bbcc466ae4
Dankan kumar <dankankumar642@gmail.com>
2022-08-24 14:35:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello world? What if this is hell that we have ended up here after living a MeAniNgfUll life in some other realm??? !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
